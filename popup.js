1c9be12d3b5c4d1739d47806a0f605950bb21760
tpinto7 <tjpinto@uwaterloo.ca>
2020-05-27 22:26:21 -0400

    Testing execute script by element id

--- popup.js
+++ popup.js
@@ -7,10 +7,9 @@
 function submitForm(){
   let color = '#e8453c';
   console.log("submitForm");
+  let fullName = $('label:contains("Full name"');
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    chrome.tabs.executeScript(null,{code: 'document.getElementById("userNameInput").value  = "Testing"'});
   });
 }
 
